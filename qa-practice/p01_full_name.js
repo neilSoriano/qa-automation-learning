fafff0de5bf0b0dc1fd37012a2a52f0254e9ff61
Neil <neilsoriano@ymail.com>
2026-06-18 14:02:54 -0700

    created practice files and new folder

---
+++ qa-practice/p01_full_name.js
@@ -0,0 +1,7 @@
+// Build a full name: join first and last name for a test user.
+
+const firstName = "Neil";
+const lastName = "Soriano";
+
+const fullName = firstName + " " + lastName;
+console.log(fullName);
